5450d62975d1f1dd0305a1cb0d6293bd8f076626
wowa1201 <stieben1201@gmail.com>
2026-09-05 15:06:15 +0200

    Projektdateien hinzufügen.

---
+++ zahlen_analysieren/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+#wowa1201
+Aufgabe: Zahlen analysieren
+Schreibe ein Programm, das den Benutzer nach 5 ganzen Zahlen fragt.
+
+Das Programm soll anschließend:
+
+die größte Zahl ausgeben
+die kleinste Zahl ausgeben
+den Durchschnitt berechnen und ausgeben
+sagen, wie viele der Zahlen gerade sind
+*/
+
+namespace Main
+{
+    public class zahlenAnalysieren
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
