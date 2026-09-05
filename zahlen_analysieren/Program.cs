338e32e4ce96fa0da11dfcd6b040fad8a6869ac8
wowa1201 <stieben1201@gmail.com>
2026-09-05 16:03:55 +0200

    code aktualierst

--- zahlen_analysieren/Program.cs
+++ zahlen_analysieren/Program.cs
@@ -23,19 +23,38 @@ namespace Main
         public static void Main(string[] args)
         {
             Console.WriteLine("geben Sie fünf ganze Zahlen ein:");
-            string zahlEins = Console.ReadLine();
-            string zahlZwei = Console.ReadLine();
-            string zahlDrei = Console.ReadLine();
-            string zahlVier = Console.ReadLine();
-            string zahlFuenf = Console.ReadLine();
-            int zahl1 = int.Parse(zahlEins);
-            int zahl2 = int.Parse(zahlZwei);
-            int zahl3 = int.Parse(zahlDrei);
-            int zahl4 = int.Parse(zahlVier);
-            int zahl5 = int.Parse(zahlFuenf);
+            Console.WriteLine("Die Zahlen dürfen nicht gleich sein");
+            Console.WriteLine("Die Zahlen dürfen nur von 1 - 10 eingegeben werden");
 
-            Console.WriteLine($"Ihre Eingaben: {zahl1}, {zahl2}, {zahl3}, {zahl4}, {zahl5}");
-
+            while (true)
+            {
+            
+                string zahlEins = Console.ReadLine();
+                string zahlZwei = Console.ReadLine();
+                string zahlDrei = Console.ReadLine();
+                string zahlVier = Console.ReadLine();
+                string zahlFuenf = Console.ReadLine();
+                int zahl1 = int.Parse(zahlEins);
+                int zahl2 = int.Parse(zahlZwei);
+                int zahl3 = int.Parse(zahlDrei);
+                int zahl4 = int.Parse(zahlVier);
+                int zahl5 = int.Parse(zahlFuenf);
+                if (zahl1 == zahl2 == zahl3 == zahl4 == zahl5)
+                {
+                    Console.WriteLine("Die Zahlen dürfen jeweil nur ein Mal vorkommen");
+                }
+                else if(zahl1 < 0 || >100 ||
+                        zahl2 < 0 || > 100 ||
+                        zahl3 < 0 || > 100 ||
+                        zahl4 < 0 || > 100 ||
+                        zahl5 < 0 || > 100)
+                {
+                    Console.WriteLine("Die Zahl darf nicht größer als 100 und nicht kleiner 0 sein");
+                }
+           
+                
+                
+            }
         }
     }
 }
