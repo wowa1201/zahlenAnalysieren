2c37575d48f4de301865088eb9102080f2265649
wowa1201 <stieben1201@gmail.com>
2026-09-05 15:34:32 +0200

    code aktualisiert

--- zahlen_analysieren/Program.cs
+++ zahlen_analysieren/Program.cs
@@ -9,7 +9,12 @@ die größte Zahl ausgeben
 die kleinste Zahl ausgeben
 den Durchschnitt berechnen und ausgeben
 sagen, wie viele der Zahlen gerade sind
+
+Bonus: Lass den Benutzer zuerst eingeben, wie viele Zahlen er eingeben möchte.
 */
+using System;
+
+
 
 namespace Main
 {
@@ -17,7 +22,20 @@ namespace Main
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Zahlen analysieren.")
+            Console.WriteLine("geben Sie fünf ganze Zahlen ein:");
+            string zahlEins = Console.ReadLine();
+            string zahlZwei = Console.ReadLine();
+            string zahlDrei = Console.ReadLine();
+            string zahlVier = Console.ReadLine();
+            string zahlFuenf = Console.ReadLine();
+            int zahl1 = int.Parse(zahlEins);
+            int zahl2 = int.Parse(zahlZwei);
+            int zahl3 = int.Parse(zahlDrei);
+            int zahl4 = int.Parse(zahlVier);
+            int zahl5 = int.Parse(zahlFuenf);
+
+            Console.WriteLine($"Ihre Eingaben: {zahl1}, {zahl2}, {zahl3}, {zahl4}, {zahl5}");
+
         }
     }
 }
